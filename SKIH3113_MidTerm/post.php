d5e2157c2bdaa60cbb1e0d727eab29ad09c43714
TONNY TANG CHIEW LIH <tonnytang01@gmail.com>
2024-06-13 18:49:48 +0800

    Update post.php

--- SKIH3113_MidTerm/post.php
+++ SKIH3113_MidTerm/post.php
@@ -1,32 +1,39 @@
 <?php
 
+// Database connection parameters
 $servername = 'localhost';
-// REPLACE with your Database name
+//  Database name
 $dbname = 'air_quality';
-// REPLACE with Database user
+// Database user
 $username = 'root';
-// REPLACE with Database user password
+// Database user password
 $password = '';
+// Predefined API key value for security
 $api_key_value = 'tPmAT5Ab3j7F9';
 
+// Initialize variables
 $api_key = $Humidity = $Temperature = $CO2lvl = $Quality = "";
 
+// Check if the request method is POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Retrieve and sanitize the API key from POST data
   $api_key = test_input($_POST["api_key"]);
+  // Verify the API key
   if ($api_key == $api_key_value) {
+    // Retrieve and sanitize the data from POST request
     $Humidity = test_input($_POST["Humidity"]);
     $Temperature = test_input($_POST["Temperature"]);
     $CO2lvl = test_input($_POST["CO2lvl"]);
     $Quality = test_input($_POST["Quality"]);
 
-    // Create connection
+    // Create a connection to the database
     $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
+    // Check if the connection failed
     if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
     }
 
-    // Prepare and bind
+    // Prepare and bind an SQL statement for inserting data
     $stmt = $conn->prepare("INSERT INTO gassensor (Humidity, Temperature, CO2lvl, Quality) VALUES (?, ?, ?, ?)");
     $stmt->bind_param("ssss", $Humidity, $Temperature, $CO2lvl, $Quality);
 
@@ -41,15 +48,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt->close();
     $conn->close();
   } else {
+    // Output error message for wrong API key
     echo "Wrong API Key provided.";
   }
 } else {
+  // Output error message for no data posted
   echo "No data posted with HTTP POST.";
 }
 
+// Function to sanitize input data
 function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
+  $data = trim($data);            // Remove whitespace from both sides
+  $data = stripslashes($data);    // Remove backslashes
+  $data = htmlspecialchars($data); // Convert special characters to HTML entities
   return $data;
 }
+?>
